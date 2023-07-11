0cb37564e5f06f98e82b7be3e41370abd6b1eddd
peter kimanzi <kimjayden001@gmail.com>
2023-07-11 19:52:31 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const cartoes = document.querySelectorAll(".cartao");
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao) {
+  cartoes[indiceCartao].classList.add("selecionado");
+}
+
+btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
