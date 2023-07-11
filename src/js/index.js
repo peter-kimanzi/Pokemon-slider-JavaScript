ec4a1e6ee6d174b3a5b7fc23bf0474629c93c334
peter kimanzi <kimjayden001@gmail.com>
2023-07-11 20:10:59 +0300

    js page corrections

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const cartoes = document.querySelectorAll(".cartao");
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
 
 
 
