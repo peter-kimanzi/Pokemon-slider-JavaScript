6b754eb43a6fe6ae83d9686501c5d888848ed762
peter kimanzi <kimjayden001@gmail.com>
2023-07-11 20:11:07 +0300

    js page corrections

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,10 @@ const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 let cartaoAtual = 0;
 
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
 
 
 
